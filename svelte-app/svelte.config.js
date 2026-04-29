25d6331cb91d65de6d2f1a4976ed42c495d20893
Carla Epifanio Vivero <148543863+carlaaev@users.noreply.github.com>
2026-04-29 06:44:19 +0100

    deploy

--- svelte-app/svelte.config.js
+++ svelte-app/svelte.config.js
@@ -2,13 +2,13 @@ import adapter from '@sveltejs/adapter-static';
 
 export default {
   kit: {
-    paths: {
-      base: '/spottoweb'
-    },
     adapter: adapter({
       pages: 'build',
       assets: 'build',
       fallback: 'index.html'
-    })
+    }),
+    paths: {
+      base: '/spottoweb'
+    }
   }
 };
